76f701dcb4473c2e58e17d01b38a5543ddb7b0c8
CHENG HAN <y3226999@gmail.com>
2020-06-06 16:33:04 +0800

    update

---
+++ data/main.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+
+fs.readFile('stock.txt', 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    var data_arr = data.split(/\s* \s*/)
+    const json = {}
+
+
+    while (data_arr.length) {
+        let i = 1
+        const sector = {}
+        while (parseInt(data_arr[i])) {
+            sector[data_arr[i]] = data_arr[i + 1]
+            i += 2
+        }
+        if (json[data_arr[0].toString()] == undefined)
+            json[data_arr[0].toString()] = sector
+        else
+            json[data_arr[0].toString() + '_'] = sector
+        // console.log(data_arr[i - 1])
+        data_arr = data_arr.slice(i)
+    }
+    // console.log(json)
+    fs.writeFile('stock_list.json', JSON.stringify(json), 'utf8', function (err2) {
+        if (err2) throw err2;
+    });
+});
--- polls/views.py
+++ polls/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from polls import final_project
 
+
 def search(request):
-    return render(request, 'search.html')
+    f = open("data/stock_list.json", "r", encoding="utf-8")
+    data = f.read()
+    f.close()
+    return render(request, 'search.html', {
+        'value': data
+    })
+
 
 def result(request):
     stock_list = request.POST.getlist('stock')
     return render(request, 'result.html', {
         'value': final_project.func(stock_list),
     })
-
